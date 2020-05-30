841a10c0031fa708522f3b2786e68da8ea6ed69f
Lucas Valhos <lucas@valhos.com>
2020-05-30 11:01:44 -0300

    github corner

--- src/modules/my/app/app.js
+++ src/modules/my/app/app.js
@@ -1,3 +1,6 @@
 import { LightningElement } from 'lwc';
 
-export default class App extends LightningElement {}
+export default class App extends LightningElement {
+    githubRepositoryUrl =
+        'https://github.com/lucasvalhos/salesforce-lightning-web-components-lwc-library';
+}
