1e09c1ac3b6467c6818da5714aaf4e9066769339
Lucas Valhos <lucas@valhos.com>
2020-05-31 10:44:08 -0300

    add LWC SOQL Builder

--- src/modules/my/componentList/componentList.js
+++ src/modules/my/componentList/componentList.js
@@ -3,6 +3,22 @@ import { LightningElement } from 'lwc';
 export default class ComponentList extends LightningElement {
     componentRecords = [
         {
+            createdAt: '2020-05-31 06:31:22',
+            name: 'LWC SOQL Builder',
+            category: 'Tools',
+            description:
+                'Awesome SOQL execution tool developed in Lightning Web Components Open Source',
+            githubRepoUrl:
+                'https://github.com/lwc-soql-builder/lwc-soql-builder',
+            playgroundUrl: 'https://lwc-soql-builder.github.io/',
+            owner: {
+                name: 'atskimura',
+                githubNickname: 'atskimura',
+                githubUrl: 'https://github.com/atskimura',
+                avatar: 'https://github.com/atskimura.png?size=40'
+            }
+        },
+        {
             createdAt: '2020-05-30 12:51:56',
             name: 'LWC Stencil',
             category: 'UI Components',
